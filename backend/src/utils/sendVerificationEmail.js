abb4a6a6db7c6145002c1204b465daf1099adf0a
majed29c <majedshmaitlu@gmail.com>
2025-06-30 18:48:10 +0300

    adding send verification email service

---
+++ backend/src/utils/sendVerificationEmail.js
@@ -0,0 +1,30 @@
+const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const sendVerificationEmail = async (email,token) => {
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+    const verificationLink = `${process.env.FRONTEND_URL}/verify-email?token=${token}`;
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: 'Email Verification',
+        html: `<p>Click the link below to verify your email:</p>
+               <a href="${verificationLink}">Verify Email</a>`
+    };
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log('Verification email sent successfully');
+    } catch (error) {
+        console.error('Error sending verification email:', error);
+        throw new Error('Failed to send verification email');
+    }
+}
